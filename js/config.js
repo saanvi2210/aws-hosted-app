5f57fd559e534f8092f0a476b729b6eb304727b9
Saanvi Manocha <142445511+saanvi2210@users.noreply.github.com>
2025-11-10 01:21:14 +0530

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_KGnZJ3enF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1pqtnhj6lj33odlqniq6dj08ff', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'h0o9vcj4ptmhfso0dbai8f91d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
